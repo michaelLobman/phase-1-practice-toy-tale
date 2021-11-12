96cb0c71000af45d4715a6cf6886caa4aa280ae7
Michael Lobman <michael.lobman@gmail.com>
2021-11-12 14:55:58 -0500

    commit tree c1b47cd6c0b1ed8f6dc08b09bf4947695df201a1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+const toysUrl = 'http://localhost:3000/toys'
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +14,80 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", loadFunctions)
+document.addEventListener("load", likeToy)
+
+function loadFunctions(){
+  fetchToys();
+  submitToy();
+}
+
+function fetchToys() {
+  fetch(toysUrl)
+  .then(resp => resp.json())
+  .then(toys => {
+    for(let toy of toys) {
+      const collection = document.querySelector('#toy-collection');
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+      <h2>${toy["name"]}</h2>
+      <img class="toy-avatar" src=${toy["image"]}>
+      <p>${toy["likes"]}</p>
+      <button class="like-btn" id=${toy["id"]}>like</button>
+      `
+      collection.append(card);
+    }
+  })
+  .then(likeToy)
+};
+
+function submitToy() {
+  const toyForm = document.querySelector('form');
+  toyForm.addEventListener('submit', postToy)
+}
+
+function postToy(e){
+  e.preventDefault();
+  let toyName = e.target.children[1].value;
+  let toyImageUrl = e.target.children[3].value;
+  fetch(toysUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      "name": toyName,
+      "image": toyImageUrl,
+      "likes": 0,
+    })
+  })  
+}
+
+function likeToy(){
+  let likeBtns = document.getElementsByClassName('like-btn');
+  for(let btn of likeBtns) {
+    btn.addEventListener('click', updateLikes)
+  }
+}
+
+function updateLikes(e){
+  let likeId = e.target["id"];
+  let totalLikes = e.target.previousElementSibling;
+  newLikes = (parseInt(totalLikes.textContent, 10) + 1).toString();
+  newLikes = newLikes.toString();
+  console.log(newLikes)
+  fetch(`${toysUrl}/${likeId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      "likes": newLikes
+    })
+  })
+  .then(() => totalLikes.textContent = newLikes)
+}
